b11bc3712ffa88b8825745ee96320e642e5208c1
ERICK <erickmxav@gmail.com>
2021-09-08 16:36:59 -0300

    start project

--- app/src/main/java/com/erickmxav/doacaodemedicamentos/activity/MainActivity.java
+++ app/src/main/java/com/erickmxav/doacaodemedicamentos/activity/MainActivity.java
@@ -1,18 +1,59 @@
 package com.erickmxav.doacaodemedicamentos.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.erickmxav.doacaodemedicamentos.R;
+import com.erickmxav.doacaodemedicamentos.config.FirebaseConfig;
+import com.erickmxav.doacaodemedicamentos.helper.UserFirebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth authentication;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Doação de Medicamentos");
+        setSupportActionBar( toolbar );
+
+        authentication = FirebaseConfig.getAuthenticationFirebase();
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch ( item.getItemId() ){
+            case R.id.logout:
+                authentication.signOut();
+                startActivity(new Intent(this, LoginActivity.class));
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
